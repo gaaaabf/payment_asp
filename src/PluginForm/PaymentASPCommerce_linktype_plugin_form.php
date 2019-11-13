d127b4c6b98a75464044215d04ff1376dfdb5c79
unknown <gabz.fernandez96@gmail.com>
2019-11-13 16:05:49 +0800

    testin

--- src/PluginForm/PaymentASPCommerce_linktype_plugin_form.php
+++ src/PluginForm/PaymentASPCommerce_linktype_plugin_form.php
@@ -77,4 +77,66 @@ class PaymentASPCommerce_linktype_plugin_form extends BasePaymentOffsiteForm {
 		);
 	}
 
+  /**
+   * Builds the redirect form.
+   *
+   * @param array $form
+   *   The plugin form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param string $redirect_url
+   *   The redirect url.
+   * @param array $data
+   *   Data that should be sent along.
+   * @param string $redirect_method
+   *   The redirect method (REDIRECT_GET or REDIRECT_POST constant).
+   *
+   * @return array
+   *   The redirect form, if $redirect_method is REDIRECT_POST.
+   *
+   * @throws \Drupal\commerce\Response\NeedsRedirectException
+   *   The redirect exception, if $redirect_method is REDIRECT_GET.
+   */
+  protected function buildRedirectForm(array $form, FormStateInterface $form_state, $redirect_url, array $data, $redirect_method = self::REDIRECT_GET) {
+    if ($redirect_method == self::REDIRECT_POST) {
+      $form['#attached']['library'][] = 'commerce_payment/offsite_redirect';
+      $form['#process'][] = [get_class($this), 'processRedirectForm'];
+      $form['#redirect_url'] = $redirect_url;
+
+      foreach ($data as $key => $value) {
+        if ($key == 'orderDetail') {
+          for ($i=0; $i != count($value); $i++) {
+            foreach ($value[$i] as $keys => $values) {
+              $form['orderDetail'.$i][$keys] = [
+                '#type' => 'textfield',
+                '#value' => $values,
+                // Ensure the correct keys by sending values from the form root.
+                '#parents' => [$keys],
+                '#size' => 3,
+              ];
+            }
+          }
+        } else {
+          $form[$key] = [
+            '#type' => 'textfield',
+            '#value' => $value,
+            // Ensure the correct keys by sending values from the form root.
+            '#parents' => [$key],
+          ];
+        }
+      }
+      // The key is prefixed with 'commerce_' to prevent conflicts with $data.
+      $form['commerce_message'] = [
+        '#markup' => '<div class="checkout-help">' . t('Please wait while you are redirected to the payment server. If nothing happens within 10 seconds, please click on the button below.') . '</div>',
+        '#weight' => -10,
+      ];
+    }
+    else {
+      $redirect_url = Url::fromUri($redirect_url, ['absolute' => TRUE, 'query' => $data])->toString();
+      throw new NeedsRedirectException($redirect_url);
+    }
+
+    return $form;
+  }
+
 }
