4b1e7d50f8c86bedff9f9e1840a1648fdabb1df0
Renier John Sediego <renierjohnsediego@gmail.com>
2019-11-15 13:28:36 +0800

    interchange utf-8 convertion to base64 conversion

--- src/PluginForm/PaymentASPCommerce_linktype_plugin_form.php
+++ src/PluginForm/PaymentASPCommerce_linktype_plugin_form.php
@@ -5,9 +5,27 @@ namespace Drupal\payment_asp\PluginForm;
 use Drupal\commerce_payment\Exception\PaymentGatewayException;
 use Drupal\commerce_payment\PluginForm\PaymentOffsiteForm as BasePaymentOffsiteForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\commerce_order\Entity\Order;
 
+class PaymentASPCommerce_linktype_plugin_form extends BasePaymentOffsiteForm   {
 
-class PaymentASPCommerce_linktype_plugin_form extends BasePaymentOffsiteForm {
+	/**
+	* {@inheritdoc}
+	*/
+	public function array_push_associative(&$arr) {
+       $args = func_get_args();
+       foreach ($args as $arg) {
+           if (is_array($arg)) {
+               foreach ($arg as $key => $value) {
+                   $arr[$key] = $value;
+                   $ret++;
+               }
+           }else{
+               $arr[$arg] = "";
+           }
+       }
+       return $ret;
+    }
 
 	/**
 	* {@inheritdoc}
@@ -20,33 +38,141 @@ class PaymentASPCommerce_linktype_plugin_form extends BasePaymentOffsiteForm {
 		/** @var \Drupal\commerce_paypal\Plugin\Commerce\PaymentGateway\ExpressCheckoutInterface $payment_gateway_plugin */
 		$payment_gateway_plugin = $payment->getPaymentGateway()->getPlugin();
 		$postdata = $payment_gateway_plugin->getOrderData($payment);
-		
 		// Set URL
 		// $postdata["success_url"] = $form['#return_url'];
-		$postdata["cancel_url"] = $form['#cancel_url'];
-		$postdata["error_url"] = $form['#cancel_url'];
+		// $postdata["cancel_url"] = $form['#cancel_url'];
+		// $postdata["error_url"] = $form['#cancel_url'];
 		// $postdata["pagecon_url"] = "http://localhost/latestmultty/payment/notify/payment_asp_link_type";
 
 
 		// Convert to UTF-8
-		$postdata = mb_convert_encoding($postdata, 'Shift_JIS', 'UTF-8');
+	//	return $form;
+		// $postdata = mb_convert_encoding($postdata, 'Shift_JIS', 'UTF-8');
 		// Concatenate each value
-		$sps_hashcode = (String) implode('', $postdata);
-		// Hashkey generation using sha1
-		$sps_hashcode = sha1($sps_hashcode);
-		// Adding hashkey to parameters to be passed
-		$postdata['sps_hashcode'] = $sps_hashcode;
+				 $first_data 	= 	$postdata['first_data'];
+				 $order_details =   $postdata['order_details'];	
+			 	 $second_data	=	$postdata['second_data'];
+ 				
+ 				 $result_order_details = [];
+ 				 $result_first_data = [];
+ 				 $result_second_data = [];	
+
+ 				 foreach ($first_data as $key => $value) {
+					 $result_first_data += [$key => $value]; 
+				}
+				
+				foreach ($order_details as $key => $value) {
+					 $result_order_details += [$key => $value]; 
+				}
+
+				foreach ($second_data as $key => $value) {
+					 $result_second_data += [$key => $value]; 
+				}
+
+	$order_details_new = [];
+	$order_details_new[0] = $result_order_details;
+	$order_details_new[1] = $result_order_details;
+
+	$result_first_data["cancel_url"] = $form['#cancel_url'];
+	$result_first_data["error_url"] = $form['#cancel_url'];
+
+			$postdata = $result_first_data +  $result_second_data  ;	
+
+	
+
+
+	$postdata = mb_convert_encoding($postdata, 'Shift_JIS', 'UTF-8');
+
+
+	foreach ($postdata as $key => $value) {
+			if (\Drupal::service('payment_asp.languageCheck')->isJapanese($postdata[$key]) || strcmp($key, 'free_csv') == 0) {
+				$postdata[$key] = base64_encode($postdata[$key]);
+		}
+	}
+	
+	$sps_hashcode = (String) implode('', $postdata);
+	// Hashkey generation using sha1
+	$sps_hashcode = sha1($sps_hashcode);
+	$postdata_freecsv = $postdata['free_csv'];
+
+		
+	
+	// Adding hashkey to parameters to be passed
+	$result_order_details_new = [];
+	foreach ($order_details as $key => $value) {
+					 $result_order_details_new += [$key => $value."new"]; 
+			}
+
+
+			$postdata = $result_first_data   +  $result_second_data ; //array($result_order_details,$result_order_details_new)
+
+	$postdata['free_csv'] = $postdata_freecsv;
+    $postdata['sps_hashcode'] = $sps_hashcode;
+	
+	
+
+	
+ // return $form;
 
 		return $this->buildRedirectForm(
 			$form,
 			$form_state,
 			// 'https://stbfep.sps-system.com/Extra/PayRequestAction.do',
-			// 'https://stbfep.sps-system.com/Extra/BuyRequestAction.do',
+	 		'https://stbfep.sps-system.com/Extra/BuyRequestAction.do',
 			// 'https://stbfep.sps-system.com/f04/FepPayInfoReceive.do',
-			'https://stbfep.sps-system.com/f01/FepBuyInfoReceive.do',
+			// 'https://stbfep.sps-system.com/f01/FepBuyInfoReceive.do',
+			// '/checkout/132/payment',
 			$postdata,
 			'post'
 		);
 	}
+
+/**
+   * Builds the redirect form.
+   *
+   * @param array $form
+   *   The plugin form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   * @param string $redirect_url
+   *   The redirect url.
+   * @param array $data
+   *   Data that should be sent along.
+   * @param string $redirect_method
+   *   The redirect method (REDIRECT_GET or REDIRECT_POST constant).
+   *
+   * @return array
+   *   The redirect form, if $redirect_method is REDIRECT_POST.
+   *
+   * @throws \Drupal\commerce\Response\NeedsRedirectException
+   *   The redirect exception, if $redirect_method is REDIRECT_GET.
+   */
+  protected function buildRedirectForm(array $form, FormStateInterface $form_state, $redirect_url, array $data, $redirect_method = self::REDIRECT_GET) {
+    if ($redirect_method == self::REDIRECT_POST) {
+      $form['#attached']['library'][] = 'commerce_payment/offsite_redirect';
+      $form['#process'][] = [get_class($this), 'processRedirectForm'];
+      $form['#redirect_url'] = $redirect_url;
+
+      foreach ($data as $key => $value) {
+
+        $form[$key] = [
+          '#type' => 'hidden',
+          '#value' => $value,
+		  '#parents' => [$key],
+        ];
+      }
+      // The key is prefixed with 'commerce_' to prevent conflicts with $data.
+      $form['commerce_message'] = [
+        '#markup' => '<div class="checkout-help">' . t('Please wait while you are redirected to the payment server. If nothing happens within 10 seconds, please click on the button below.') . '</div>',
+        '#weight' => -10,
+      ];
+    }
+    else {
+      $redirect_url = Url::fromUri($redirect_url, ['absolute' => TRUE, 'query' => $data])->toString();
+      throw new NeedsRedirectException($redirect_url);
+    }
+
+    return $form;
+  }	
 
 }
