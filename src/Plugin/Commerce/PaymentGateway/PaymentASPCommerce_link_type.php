285813c165a82240e5181363e6578564679d75aa
Renier John Sediego <renierjohnsediego@gmail.com>
2019-11-15 13:27:44 +0800

    add zem kaku request, change variable in free_csv last_name and first_name

--- src/Plugin/Commerce/PaymentGateway/PaymentASPCommerce_link_type.php
+++ src/Plugin/Commerce/PaymentGateway/PaymentASPCommerce_link_type.php
@@ -136,8 +136,8 @@ class PaymentASPCommerce_link_type extends OffsitePaymentGatewayBase implements 
 		
 
 	 $payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
-	 $order_id = str_replace("aaorderID12345", "", $request->get('order_id'));
-
+	 $order_id = str_replace("zenss", "", $request->get('order_id'));
+    
 	 if($request->get('res_result') == 'OK'){
 		  	$payment = $payment_storage->create([
 			  'state' 			=> 'pending',
@@ -242,6 +242,7 @@ class PaymentASPCommerce_link_type extends OffsitePaymentGatewayBase implements 
 	* Gets the order data from Controller
 	*/
 	public function getOrderData(PaymentInterface $payment) {
+
 	$payment_storage = $this->entityTypeManager->getStorage('commerce_payment');
 
     $pc = \Drupal::service('payment_asp.PaymentASPController');
@@ -253,9 +254,17 @@ class PaymentASPCommerce_link_type extends OffsitePaymentGatewayBase implements 
 	    $givenName 				   = $order->getData("billing_profile_givenName");
 	    $familyName 			   = $order->getData("billing_profile_familyName");
 	    $payment_gateway_parameter = $order->getData("payment_gateway_parameter");
+
+
+	    $kana_givenName = mb_convert_kana($givenName, "NRS","UTF-8");
+	    $kana_familyName = mb_convert_kana($familyName, "NRS","UTF-8");
+
+// ksm($kana_givenName);
+// ksm($kana_familyName);
+
 	switch ($this->configuration['method_type']) {
 			case 'webcvs':
-				$free_csv = "LAST_NAME=,FIRST_NAME=,MAIL=" . $order->getEmail(). ",TEL=" . $payment_gateway_parameter . ",BILL_DATE=" . (int)$BILL_DATE;		
+				$free_csv = "LAST_NAME=".$kana_givenName.",FIRST_NAME=".$kana_familyName.",MAIL=" . $order->getEmail(). ",TEL=" . $payment_gateway_parameter . ",BILL_DATE=" . (int)$BILL_DATE;		
 				break;
 			case 'credit3d':
 				// $payment_gateway_parameter  = (int) $order->getData("payment_gateway_parameter");
@@ -272,54 +281,57 @@ class PaymentASPCommerce_link_type extends OffsitePaymentGatewayBase implements 
 		}
     
 		$postdata = [
+			"first_data"		=> [
+					'pay_method'		=> $this->configuration['method_type'],
+					'merchant_id'		=> $this->configuration['merchant_id'],
+					'service_id'		=> $this->configuration['service_id'],
+					"cust_code"			=> $orderData['cust_code'],
+					"sps_cust_no"		=> "",
+					"sps_payment_no"	=> "",
+					"order_id"			=> $orderData['order_id']."zenss",
+					"item_id"			=> $orderData['orderDetail'][0]["dtl_item_id"],
+					"pay_item_id"		=> "",
+					"item_name"			=> "",//$orderData['orderDetail'][0]["dtl_item_name"],
+					"tax"				=> "0",//$orderData['tax'],
+					"amount"			=> $orderData['amount'],
+					"pay_type"			=> isset($pay_type) ? $pay_type : "0",
+					"auto_charge_type"	=> isset($auto_charge_type) ? $auto_charge_type : "",
+					"service_type"		=> "0",
+					"div_settele"		=> isset($div_settele) ? $div_settele : "",
+					"last_charge_month"	=> isset($last_charge_month) ? $last_charge_month : "",
+					"camp_type"			=> "",
+					"tracking_id"		=> "",
+					"terminal_type"		=> "0",
+					"success_url"		=> $this->getReturnUrl($id)->toString(),
+					"cancel_url"		=> "",
+					"error_url"			=> "",
+					"pagecon_url"		=> $this->getNotifyUrl()->toString(),
+					"free1"				=> "",
+					"free2"				=> "",
+					"free3"				=> "",
+					"free_csv"			=> isset($free_csv) ? $free_csv : '',
+									],
 
-			'pay_method'		=> $this->configuration['method_type'],
-			'merchant_id'		=> $this->configuration['merchant_id'],
-			'service_id'		=> $this->configuration['service_id'],
-			"cust_code"			=> $orderData['cust_code'],
-			"sps_cust_no"		=> "",
-			"sps_payment_no"	=> "",
-			"order_id"			=> $orderData['order_id'],
-			"item_id"			=> $orderData['orderDetail'][0]["dtl_item_id"],
-			"pay_item_id"		=> "",
-			"item_name"			=> "",//$orderData['orderDetail'][0]["dtl_item_name"],
-			"tax"				=> "0",//$orderData['tax'],
-			"amount"			=> $orderData['amount'],
-			"pay_type"			=> isset($pay_type) ? $pay_type : "0",
-			"auto_charge_type"	=> isset($auto_charge_type) ? $auto_charge_type : "",
-			"service_type"		=> "0",
-			"div_settele"		=> isset($div_settele) ? $div_settele : "",
-			"last_charge_month"	=> isset($last_charge_month) ? $last_charge_month : "",
-			"camp_type"			=> "",
-			"tracking_id"		=> "",
-			"terminal_type"		=> "0",
-			"success_url"		=> $this->getReturnUrl($id)->toString(),
-			"cancel_url"		=> "",
-			"error_url"			=> "",
-			"pagecon_url"		=> $this->getNotifyUrl()->toString(),
-			"free1"				=> "",
-			"free2"				=> "",
-			"free3"				=> "",
-			"free_csv"			=> isset($free_csv) ? $free_csv : '',	
-			// 'dtl_rowno'			=> $orderData['orderDetail'][0]["dtl_rowno"],
-			// 'dtl_item_id'		=> $orderData['orderDetail'][0]["dtl_item_id"],
-			// 'dtl_item_name'		=> $orderData['orderDetail'][0]["dtl_item_name"],
-			// 'dtl_item_count'	=> $orderData['orderDetail'][0]["dtl_item_count"],
-			// 'dtl_tax'			=> $orderData['orderDetail'][0]["dtl_tax"],
-			// 'dtl_amount'		=> $orderData['orderDetail'][0]["dtl_amount"],
-			// 'dtl_free1'			=> $orderData['orderDetail'][0]["dtl_free1"],
-			// 'dtl_free2'			=> $orderData['orderDetail'][0]["dtl_free2"],
-			// 'dtl_free3'			=> $orderData['orderDetail'][0]["dtl_free3"],
-			'request_date'		=> date("YmdGis"),
-			"limit_second"		=> "",
-			"hashkey"			=> $this->configuration['hashkey'],
+			"order_details"		=> [
+					'dtl_rowno'			=> $orderData['orderDetail'][0]["dtl_rowno"],
+					'dtl_item_id'		=> $orderData['orderDetail'][0]["dtl_item_id"],
+					'dtl_item_name'		=> $orderData['orderDetail'][0]["dtl_item_name"],
+					'dtl_item_count'	=> $orderData['orderDetail'][0]["dtl_item_count"],
+					'dtl_tax'			=> $orderData['orderDetail'][0]["dtl_tax"],
+					'dtl_amount'		=> $orderData['orderDetail'][0]["dtl_amount"],
+					'dtl_free1'			=> $orderData['orderDetail'][0]["dtl_free1"],
+					'dtl_free2'			=> $orderData['orderDetail'][0]["dtl_free2"],
+					'dtl_free3'			=> $orderData['orderDetail'][0]["dtl_free3"],
+									],
+
+			"second_data"		=> [
+					'request_date'		=> date("YmdGis"),
+					"limit_second"		=> "",
+					"hashkey"			=> $this->configuration['hashkey'],
+									],
 		];
 
-	foreach ($postdata as $key => $value) {
-			if (\Drupal::service('payment_asp.languageCheck')->isJapanese($postdata[$key]) || strcmp($key, 'free_csv') == 0) {
-				$postdata[$key] = base64_encode($postdata[$key]);
-			}
-		}
+	
 
 		return $postdata;
 	}
